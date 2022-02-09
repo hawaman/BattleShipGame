f87c77132fc4d9427313bd37d2c1fa87c7a534e4
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:13:04 +1100

    Validation according game rule added when placing the ship

--- BattleShip/Implementation/ShipPlacer.cs
+++ BattleShip/Implementation/ShipPlacer.cs
@@ -9,13 +9,35 @@ namespace BattleShipGame.Implementation
     {
         public void AddShipToBoard(Ship ship, Board board, int row, int column)
         {
+            Validate(ship, board, row, column);
             // lets make the boardcellstatus ocupied for those cells that ship occupies
             for (int i = 0; i < ship.Size; i++)
             {
-                board.BoardCellStatuses[row, column+i] = BoardCellStatus.Occupied;
+                board.BoardCellStatuses[row, column + i] = BoardCellStatus.Occupied;
                 board.OccupationCount++;
             }
         }
-       
+        private void Validate(Ship ship, Board board, int row, int column)
+        {
+            string errorMsg = "Position of ship on the board is out of bound";
+            //check if both row column coordinates of is inside the board
+            if (row > board.Rows)
+            {
+                throw new IndexOutOfRangeException(errorMsg);
+            }
+            else if (column > board.Columns)
+            {
+                throw new IndexOutOfRangeException(errorMsg);
+            }
+            //
+            for (int s = 0; s < ship.Size; s++)
+            {
+                if (column + s > board.Columns)
+                {
+                    throw new Exception(errorMsg);
+                }
+            }
+
+        }
     }
 }
