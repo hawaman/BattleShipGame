b1a0b5952f633383102b2b435685858ae6da267e
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:07:52 +1100

    Place Ship on the board functionality

---
+++ BattleShip/Implementation/ShipPlacer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BattleShipGame.Interfaces;
+using BattleShipGame.Models;
+using BattleShipGame.Enums;
+
+namespace BattleShipGame.Implementation
+{
+    public class ShipPlacer : IShipPlacer
+    {
+        public void AddShipToBoard(Ship ship, Board board, int row, int column)
+        {
+            // lets make the boardcellstatus ocupied for those cells that ship occupies
+            for (int i = 0; i < ship.Size; i++)
+            {
+                board.BoardCellStatuses[row, column+i] = BoardCellStatus.Occupied;
+                board.OccupationCount++;
+            }
+        }
+       
+    }
+}
