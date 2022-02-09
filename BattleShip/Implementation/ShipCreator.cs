70a8f8b1df1c7ed5ee96e536c1dd04d98fbd13cd
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:12:14 +1100

    Ship creater funtionality added

---
+++ BattleShip/Implementation/ShipCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BattleShipGame.Enums;
+using BattleShipGame.Interfaces;
+using BattleShipGame.Models;
+using BattleShipGame.Models.Ships;
+
+namespace BattleShipGame.Implementation
+{
+    public class ShipCreator : IShipCreator
+    {
+        public Ship CreateShip(ShipType shipType)
+        {
+            try
+            {
+                switch (shipType)
+                {
+                    case ShipType.Carrier:
+                        return new Carrier();
+                    case ShipType.Submarine:
+                        return new Submarine();
+                    default:
+                        return new Carrier();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ship creation failed:{ex.Message}");
+            }
+        }
+    }
+}
