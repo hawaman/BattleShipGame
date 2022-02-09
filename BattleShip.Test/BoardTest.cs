55c41853ae5c86a71c0cb9b3c2c0fd8ef65ca3f2
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:21:52 +1100

    Board creating and Ship placing test cases build with some test cases

---
+++ BattleShip.Test/BoardTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BattleShipGame.Enums;
+using BattleShipGame.Implementation;
+using Xunit;
+
+namespace BattleShip.Test
+{
+    public class BoardTest
+    {
+        [Theory]
+        [InlineData(10, 10)]
+        [InlineData(20, 20)]
+        [InlineData(30, 30)]
+        [InlineData(40, 40)]
+        public void shouldReturnValidBoardWhenBoardCreated(
+             int boardRows,
+             int boardColumns)
+        {
+            // arrage
+            var boardCreator = new BoardCreator();
+
+            //action
+            var board = boardCreator.CreateBoard(boardRows, boardColumns);
+
+            //assert
+            Assert.Equal(boardRows * boardColumns,board.BoardCellStatuses.Length);
+        }
+    }
+}
---
+++ BattleShip.Test/ShipPlacerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BattleShipGame.Enums;
+using BattleShipGame.Implementation;
+using Xunit;
+
+namespace BattleShip.Test
+{
+    public class ShipPlacerTest
+    {
+        [Theory]
+        [InlineData(10, 10, 0, 0, ShipType.Carrier)]
+        [InlineData(10, 10, 0, 1, ShipType.Submarine)]
+        [InlineData(10, 10, 0, 2, ShipType.Submarine)]
+        [InlineData(10, 10, 0, 3, ShipType.Submarine)]
+        public void shouldPlaceShipWhenCorrectPositionProvided(
+            int boardRows,
+            int boardColumns,
+            int placementRow,
+            int placementColumn,
+            ShipType shipType)
+        {
+            // arrage
+            var boardCreator = new BoardCreator();
+            var board = boardCreator.CreateBoard(boardRows, boardColumns);
+
+
+            var shipCreator = new ShipCreator();
+            var ship = shipCreator.CreateShip(shipType);
+
+            // act
+            var shipPlacer = new ShipPlacer();
+            shipPlacer.AddShipToBoard(ship, board, placementRow, placementColumn);
+            //assert
+            Assert.True(board.BoardCellStatuses[placementRow, placementColumn] == BoardCellStatus.Occupied &&
+                board.BoardCellStatuses[placementRow, placementColumn + ship.Size -1] == BoardCellStatus.Occupied);
+        }
+    }
+}
