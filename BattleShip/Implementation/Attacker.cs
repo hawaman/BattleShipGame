8b7083838f41ad311749ffba164c325154b191e5
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:14:20 +1100

    Attack functionality added that returns hit or miss

---
+++ BattleShip/Implementation/Attacker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BattleShipGame.Enums;
+using BattleShipGame.Interfaces;
+using BattleShipGame.Models;
+
+namespace BattleShipGame.Implementation
+{
+    public class Attacker : IAttacker
+    {
+        public AttackStatus Attack(Board board, int row, int column)
+        {
+           if (board.BoardCellStatuses[row,column]== BoardCellStatus.Occupied || board.BoardCellStatuses[row, column] == BoardCellStatus.Hit)
+            {
+                board.BoardCellStatuses[row, column] = BoardCellStatus.Hit;
+                board.HitCount++;
+                return AttackStatus.Hit;
+            }
+            board.BoardCellStatuses[row, column] = BoardCellStatus.Miss;
+            return AttackStatus.Miss;
+        }
+    }
+}
