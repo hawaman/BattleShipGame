d5b3400ac505689ba0a26a5f7aa41990fec85fb8
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:03:19 +1100

    Two new ships are created

---
+++ BattleShip/Models/Ships/Carrier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BattleShipGame.Models.Ships
+{
+    public class Carrier : Ship
+    {
+        public Carrier()
+        {
+            Size = 5;
+            ShipType = Enums.ShipType.Carrier;
+        }
+    }
+}
---
+++ BattleShip/Models/Ships/Submarine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BattleShipGame.Models.Ships
+{
+    public class Submarine: Ship
+    {
+        public Submarine()
+        {
+            Size = 2;
+            ShipType = Enums.ShipType.Submarine;
+        }
+    }
+}
