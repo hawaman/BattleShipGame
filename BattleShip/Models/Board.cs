76705d77779722c10bad440589b1799144f17f2a
gurungs <shoman.gurung@news.com.au>
2022-02-09 20:02:36 +1100

    Data models foe Ship and Board created

---
+++ BattleShip/Models/Board.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BattleShipGame.Enums;
+namespace BattleShipGame.Models
+{
+    public class Board
+    {
+        public BoardCellStatus[,] BoardCellStatuses { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public int OccupationCount { get; set; }
+        public int HitCount { get; set; }
+        public bool Lost => HitCount >= OccupationCount;
+    }
+}
---
+++ BattleShip/Models/Ship.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BattleShipGame.Enums;
+namespace BattleShipGame.Models
+{
+    public abstract class Ship
+    {
+        public ShipType ShipType { get; set; }
+        public int Size { get; set; }
+
+    }
+}
